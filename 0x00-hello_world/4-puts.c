a3b3eac80c127103c85815f0a8cd93df8025686c
FrancisTheWolf <francisnjoku023@gmail.com>
2022-09-09 03:53:08 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: 0
+ *
+ * */
+int main(void)
+{
+                   puts("\"Programming is like building amultilingual puzzle");
+
+		   return (0);
+}
